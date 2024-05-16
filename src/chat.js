59a4668582cebd625df1529b44dad0d26897ea8d
CiceroIsBack <dev@ciceroisback.com>
2024-05-16 12:56:00 -0700

    added gemini support

--- src/chat.js
+++ src/chat.js
@@ -4,31 +4,36 @@ const fs = require("fs");
 require("dotenv").config();
 const gptResponse = require("./utils/gpt");
 const anthropicResponse = require("./utils/anthropic");
+const geminiResponse = require("./utils/gemini");
 
 const chat = async (message) => {
   try {
     const channel = message.channel;
     let chatMessages = [];
     await channel.messages.fetch({ limit: 100 }).then(async (messages) => {
-        //Iterate through the messages here with the variable "messages".
-        messages.forEach(message => {
-            const role = message.author.bot ? "assistant" : "user";
-            chatMessages.push({role, content: message.content});
-        })
-      })
+      //Iterate through the messages here with the variable "messages".
+      messages.forEach((message) => {
+        const role = message.author.bot ? "assistant" : "user";
+        chatMessages.push({ role, content: message.content });
+      });
+    });
     chatMessages = chatMessages.reverse();
 
-    const infoMessage = await message.channel.send("Loading...")
+    const infoMessage = await message.channel.send("Loading...");
 
-    let {chatbotToUse} = JSON.parse(fs.readFileSync("preferences.json"));
+    let { chatbotToUse } = JSON.parse(fs.readFileSync("preferences.json"));
 
     let newMessage;
     if (chatbotToUse === "gpt") {
       newMessage = await gptResponse(chatMessages);
-    } else if (chatbotToUse === "anthropic") {
+    } else if (chatbotToUse === "claude") {
       newMessage = await anthropicResponse(chatMessages);
+    } else if (chatbotToUse === "gemini") {
+      newMessage = await geminiResponse(chatMessages);
     } else {
-      throw new Error("Invalid chatbot to use. Please set the CHATBOT_TO_USE environment variable to 'gpt' or 'anthropic'");
+      throw new Error(
+        "Invalid chatbot to use. Please set the CHATBOT_TO_USE environment variable to 'gpt' or 'anthropic'"
+      );
     }
 
     // split newMessage after 1900 characters, if newMessage is more than 2000 long
@@ -41,11 +46,8 @@ const chat = async (message) => {
       message.channel.send(newMessage);
     }
 
-
-
     infoMessage.delete();
     // message.channel.send(newMessage);
-    
   } catch (err) {
     logError(err, message);
   }
---
+++ src/utils/gemini.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+async function geminiResponse(chatMessages) {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  //pull off the most recent message to send separately
+  const msg = chatMessages.pop().content;
+
+  // Convert the chatMessages to the format expected by the model
+  chatMessages.forEach((message) => {
+    message.parts = [{ text: message.content }];
+    delete message.content;
+    if (message.role === "assistant") {
+        message.role = "model"
+    }
+  });
+
+  const chat = model.startChat({
+    history: chatMessages,
+    generationConfig: {
+      maxOutputTokens: 4000,
+    },
+  });
+
+  const result = await chat.sendMessage(msg);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+  return text;
+}
+
+module.exports = geminiResponse;
