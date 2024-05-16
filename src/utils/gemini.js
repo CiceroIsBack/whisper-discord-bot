536f942f17cd97c8a7a1fb5ac2d126d486ea6663
CiceroIsBack <dev@ciceroisback.com>
2024-05-16 12:57:40 -0700

    removed logging

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -31,7 +31,6 @@ async function geminiResponse(chatMessages) {
   const result = await chat.sendMessage(msg);
   const response = await result.response;
   const text = response.text();
-  console.log(text);
   return text;
 }
 
