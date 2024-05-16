b54760116100f38660f98432baf0449b6918108f
CiceroIsBack <dev@ciceroisback.com>
2024-05-16 14:01:02 -0700

    now changing model changes channel name

--- src/utils/useChatbot.js
+++ src/utils/useChatbot.js
@@ -10,8 +10,11 @@ const useChatbot = async (message) => {
     preferences.chatbotToUse = chatbotToUse;
     fs.writeFileSync('preferences.json', JSON.stringify(preferences, null, 2));
     
+    message.channel.edit({name:`chat-${chatbotToUse}`, topic: `Directly talk w/ ${chatbotToUse}. Use !clear to reset, and !use gpt|claude|gemini to set model.`});
+
     const infoMessage = await message.channel.send(`Switched to ${chatbotToUse}`);
     message.delete();
+
     setTimeout(() => {
       infoMessage.delete();
     }, 1000);
